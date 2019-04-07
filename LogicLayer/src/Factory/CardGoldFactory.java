f64183fe34d8831e12d084e2f2907fef08959891
Jose <jafjdev@gmail.com>
2019-04-07 17:11:53 -0400

    [CardGoldFactory] Working

--- LogicLayer/src/Factory/CardGoldFactory.java
+++ LogicLayer/src/Factory/CardGoldFactory.java
@@ -1,5 +1,6 @@
 package Factory;
 
+import Models.Card.CardGold;
 import Models.Card.ICard;
 
 import java.util.ArrayList;
@@ -10,53 +11,43 @@ public class CardGoldFactory extends CardFactory {
         super(cardList);
     }
 
-    @Override
     public ICard crearUno() {
-        return null;
+        return new CardGold(1, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearDos() {
-        return null;
+        return new CardGold(2, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearTres() {
-        return null;
+        return new CardGold(3, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearCuatro() {
-        return null;
+        return new CardGold(4, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearCinco() {
-        return null;
+        return new CardGold(5, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearSeis() {
-        return null;
+        return new CardGold(6, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearSiete() {
-        return null;
+        return new CardGold(7, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearDiez() {
-        return null;
+        return new CardGold(10, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearOnce() {
-        return null;
+        return new CardGold(11, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearDoce() {
-        return null;
+        return new CardGold(12, "rutaImg", false, false);
     }
 }
