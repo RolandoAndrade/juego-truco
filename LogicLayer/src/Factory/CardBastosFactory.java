cd11e6aa2dd5ea1fef2a43de12603ff860576ea1
Jose <jafjdev@gmail.com>
2019-04-07 17:11:32 -0400

    [CardBastosFactory] Working

--- LogicLayer/src/Factory/CardBastosFactory.java
+++ LogicLayer/src/Factory/CardBastosFactory.java
@@ -17,53 +17,45 @@ public class CardBastosFactory extends CardFactory {
         super.crearTodos();
     }
 
-    @Override
     public ICard crearUno() {
         return new CardBastos(1, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearDos() {
         return new CardBastos(2, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearTres() {
         return new CardBastos(3, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearCuatro() {
         return new CardBastos(4, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearCinco() {
-        return new CardBastos(5,"rutaImg",false,false);
+        return new CardBastos(5, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearSeis() {
-        return new CardBastos(6,"rutaImg",false,false);
+        return new CardBastos(6, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearSiete() {
-        return new CardBastos(7,"rutaImg",false,false);
+        return new CardBastos(7, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearDiez() {
-        return new CardBastos(10,"rutaImg",false,false);
+        return new CardBastos(10, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearOnce() {
-        return new CardBastos(11,"rutaImg",false,false);
+        return new CardBastos(11, "rutaImg", false, false);
     }
 
-    @Override
     public ICard crearDoce() {
-        return new CardBastos(12,"rutaImg",false,false);
+        return new CardBastos(12, "rutaImg", false, false);
     }
+
+
 }
