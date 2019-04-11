ad8a2ed7245fe03f743164f4b2bb8502707da1db
Jose <jafjdev@gmail.com>
2019-04-11 19:02:49 -0400

    [CARD] se edito el modelo de la carta
    Se eliminaros los bastos,copas,oro y espadas. ademas de sus fabricas y los comandos para llenar el mazo y su fabrica.

--- Common/src/Models/Card/CardSword.java
+++
@@ -1,9 +0,0 @@
-package Models.Card;
-
-public class CardSword extends Card
-{
-    public CardSword(int number, String image, boolean isPerico, boolean isPerica)
-    {
-        super(number, image, isPerico, isPerica);
-    }
-}
