6d8eaa71efc5d5b74e07c6d5f154083323279d5a
Jose <jafjdev@gmail.com>
2019-04-16 19:15:49 -0400

    [Game] Se cambio el nombre de Game a GameRoom y se agregan/obtienen plyr

--- Common/src/main/java/Models/Game/Game.java
+++
@@ -1,5 +0,0 @@
-package Models.Game;
-
-public class Game
-{
-}
--- Common/src/main/java/Models/Game/PlayGame.java
+++ Common/src/main/java/Models/Game/PlayGame.java
@@ -1,5 +1,12 @@
 package Models.Game;
 
+import Models.Player.PlayPlayer;
+
+import java.util.ArrayList;
+
 public interface PlayGame
 {
+    ArrayList<PlayPlayer> getPlayers();
+    PlayPlayer getPlayer(int i);
+    void setPlayers(ArrayList<PlayPlayer> players);
 }
