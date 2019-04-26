702f399eae5a31325dfb18817aa60f339034aa30
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-04-26 16:57:36 -0400

    [GraphicCard] Graficar cartas

--- UILayer/src/main/java/Board.java
+++ UILayer/src/main/java/Board.java
@@ -7,11 +7,17 @@ import java.awt.*;
 public class Board extends JPanel
 {
     private GraphicCard card;
+    private GraphicCard card1;
+    private GraphicCard card2;
     public Board()
     {
         this.setBackground(new Color(33, 33, 33));
         Card card=new Card(1, "sword",new OneCardSword());
-        this.card=new GraphicCard(card,true);
+        this.card=new GraphicCard(card,250,300, true);
+        card=new Card(6, "coarse",new OneCardSword());
+        this.card1=new GraphicCard(card,320,300, true);
+        card=new Card(3, "gold",new OneCardSword());
+        this.card2=new GraphicCard(card,390,300, true);
     }
     
     @Override
@@ -19,5 +25,7 @@ public class Board extends JPanel
     {
         super.paint(g);
         this.card.paint(g,this);
+        this.card1.paint(g,this);
+        this.card2.paint(g,this);
     }
 }
--- UILayer/src/main/java/GraphicCard.java
+++ UILayer/src/main/java/GraphicCard.java
@@ -5,20 +5,21 @@ import java.awt.image.ImageObserver;
 
 public class GraphicCard implements Drawable
 {
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 100;
     private Card card;
     private Image image;
     private int x,y,w,h;
     
-    public GraphicCard(Card card, boolean front)
+    public GraphicCard(Card card, int x, int y, boolean front)
     {
         this.card = card;
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         this.image=toolkit.getImage("UILayer/src/main/resources/"+card.getImagePath());
-        System.out.println(card.getImagePath());
-        this.x=0;
-        this.y=0;
-        this.w=80;
-        this.h=100;
+        this.x=x;
+        this.y=y;
+        this.w=WIDTH;
+        this.h=HEIGHT;
     }
     
     @Override
