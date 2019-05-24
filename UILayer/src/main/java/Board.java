9b42a99d22c9620d7ee8df053ea06d7f5395fa1d
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-05-24 19:23:05 -0400

    [GraphicPlayer] Nombre de los jugadores

--- UILayer/src/main/java/Board.java
+++ UILayer/src/main/java/Board.java
@@ -1,4 +1,6 @@
 import Models.Card.Card;
+import Models.Player.PlayPlayer;
+import Models.Player.Player;
 import Services.Strategies.Cards.OneCardSword;
 
 import javax.swing.*;
@@ -6,26 +8,32 @@ import java.awt.*;
 
 public class Board extends JPanel
 {
-    private GraphicCard card;
-    private GraphicCard card1;
-    private GraphicCard card2;
+    private Drawable player0;
+    private Drawable player1;
+    private Drawable player2;
+    private Drawable player3;
     public Board()
     {
         this.setBackground(new Color(33, 33, 33));
-        Card card=new Card(1, "sword",new OneCardSword());
+        this.player0=new GraphicPlayer(0,new Player("ROLANDO", new GraphicHand()));
+        this.player1=new GraphicPlayer(1,new Player("JOSÉ", new GraphicHand()));
+        this.player2=new GraphicPlayer(2,new Player("ANDRADE", new GraphicHand()));
+        this.player3=new GraphicPlayer(3,new Player("FERNÁNDEZ", new GraphicHand()));
+        /*Card card=new Card(1, "sword",new OneCardSword());
         this.card=new GraphicCard(card,250,300, true);
         card=new Card(6, "coarse",new OneCardSword());
         this.card1=new GraphicCard(card,320,300, false);
         card=new Card(3, "gold",new OneCardSword());
-        this.card2=new GraphicCard(card,390,300, true);
+        this.card2=new GraphicCard(card,390,300, true);*/
     }
     
     @Override
     public void paint(Graphics g)
     {
         super.paint(g);
-        this.card.paint(g,this);
-        this.card1.paint(g,this);
-        this.card2.paint(g,this);
+        player0.paint(g, this);
+        player1.paint(g, this);
+        player2.paint(g, this);
+        player3.paint(g, this);
     }
 }
--- UILayer/src/main/java/GraphicPlayer.java
+++ UILayer/src/main/java/GraphicPlayer.java
@@ -5,14 +5,39 @@ import java.awt.image.ImageObserver;
 public class GraphicPlayer implements Drawable
 {
     private PlayPlayer player;
-    public GraphicPlayer(PlayPlayer player)
+    private int number, x,y;
+    private static final int FONT_SIZE=15;
+    public GraphicPlayer(int number, PlayPlayer player)
     {
+        this.number = number;
         this.player = player;
+        int len = player.getName().length();
+        switch (number)
+        {
+            case 0:
+                this.x=10;
+                this.y=460;
+                break;
+            case 1:
+                this.x=690-20*(len)+(len-1)*10;
+                this.y=460;
+                break;
+            case 2:
+                this.x=690-20*(len)+(len-1)*10;
+                this.y=25;
+                break;
+            case 3:
+                this.x=10;
+                this.y=25;
+                break;
+        }
     }
     
     @Override
     public void paint(Graphics g, ImageObserver observer)
     {
+        g.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        g.drawString(this.player.getName(),this.x,this.y);
         try
         {
             ((Drawable)player.getHand()).paint(g,observer);
