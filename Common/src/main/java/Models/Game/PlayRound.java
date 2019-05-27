1adb6f1c3249a60858e1df07657c4f13bf5e730f
OscarM <oscarmarquez2218@gmail.com>
2019-05-26 21:39:11 -0400

    Empezando idea de las rondas

---
+++ Common/src/main/java/Models/Game/PlayRound.java
@@ -0,0 +1,11 @@
+package Models.Game;
+
+import Models.Deck.PlayDeck;
+import Models.Player.PlayPlayer;
+import java.util.ArrayList;
+
+public interface PlayRound{
+
+    ArrayList<PlayPlayer> getPlayers();
+
+}
---
+++ Common/src/main/java/Models/Game/Round.java
@@ -0,0 +1,10 @@
+package Models.Game;
+
+import Models.Deck.PlayDeck;
+import Models.Player.PlayPlayer;
+import java.util.ArrayList;
+
+public class Round implements PlayRound{
+
+
+}
