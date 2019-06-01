923029e721919303334ecc17b3c89306f96330f0
OscarM <oscarmarquez2218@gmail.com>
2019-05-31 22:32:27 -0400

    Jugar ronda individual agregada

--- Common/src/main/java/Models/Game/PlayRound.java
+++ Common/src/main/java/Models/Game/PlayRound.java
@@ -1,11 +1,8 @@
 package Models.Game;
 
-import Models.Deck.PlayDeck;
 import Models.Player.PlayPlayer;
-import java.util.ArrayList;
 
 public interface PlayRound{
 
-    ArrayList<PlayPlayer> getPlayers();
-
+    PlayPlayer getWinnerRound();
 }
