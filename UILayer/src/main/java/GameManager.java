a2522558afee0f90a4494197b4c2e90143e52cca
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-06-20 19:37:42 -0400

    [GameManager] Manejo de empates

--- UILayer/src/main/java/GameManager.java
+++ UILayer/src/main/java/GameManager.java
@@ -12,6 +12,13 @@ public class GameManager
     private static Command fill;
     private static FrameControl frameControl;
     
+    private static int globalScoreTeamA=0;
+    private static int globalScoreTeamB=0;
+    
+    private static int TeamAScore=0;
+    private static int TeamBScore=0;
+    
+    private static int trickRate=1;
     
     public static boolean isMyTurn(int numberOfPlayer)
     {
@@ -26,8 +33,15 @@ public class GameManager
         if (NUMBER_OF_TURNS==4)
         {
             TURN_OF_PLAYER=whoWins();
+            if(TURN_OF_PLAYER>-1)
+            {
+                System.out.println("Gana jugador "+gameRoom.getPlayer(TURN_OF_PLAYER).getName());
+            }
+            else
+            {
+                System.out.println("Empate");
+            }
             NUMBER_OF_TURNS=0;
-            System.out.println("Gana Jugador "+TURN_OF_PLAYER);
             endRound();
         }
     }
@@ -65,7 +79,37 @@ public class GameManager
                 winner=i;
             }
         }
-        return !draw?winner:0;
+        if(!draw)
+        {
+            if(winner%2==0)
+                TeamAScore+=trickRate;
+            else
+                TeamBScore+=trickRate;
+            return winner;
+        }
+        return resolveDraw();
+    }
+    
+    private static int resolveDraw()
+    {
+        int p0=cards[0].getPoints();
+        int p1=cards[1].getPoints();
+        int p2=cards[2].getPoints();
+        int p3=cards[3].getPoints();
+        int TeamA[]={Math.max(p0,p2),Math.min(p0,p2)};
+        int TeamB[]={Math.max(p1,p3),Math.min(p1,p3)};
+        if(TeamA[0]>TeamB[0])
+        {
+            //Empate entre compañeros
+            return 0;
+        }
+        else if(TeamB[0]>TeamA[0])
+        {
+            //Empate entre compañeros
+            return 1;
+        }
+        return -1;
+        
     }
     
     public static void setFill(Command fill)
